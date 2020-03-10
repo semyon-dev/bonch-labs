f17a5ee14c0f0168921a9dcae5f641b0d1976c95
semyon-dev <semyon-dev@protonmail.com>
2020-03-11 01:40:30 +0300

    cmake updated

--- Programming/lab9/lab9.c
+++ Programming/lab9/lab9.c
@@ -10,7 +10,7 @@ void input() {
     int rowLocal;
     printf("Введите кол-во вводимых строк: ");
     scanf("%i", &rowLocal);
-    fgets(text[0], max, stdin); // костыль
+    fgets(text[0], max, stdin);
     for (int i = 0; i < rowLocal; i++) {
         fgets(text[i], max, stdin);
     }
