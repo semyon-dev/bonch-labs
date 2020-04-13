873ffe46f23fe55d5004bde9339557a74ce1ad6a
semyon-dev <semyon-dev@protonmail.com>
2020-04-13 22:09:30 +0300

    MDPL lab1 funcs for all types added

--- MDPL/lab1/main.c
+++ MDPL/lab1/main.c
@@ -1,21 +1,66 @@
 #include <stdio.h>
 
-extern int mainA();
+extern int int8();
 
-__uint8_t a, b, c;
+extern int uint8();
+
+extern int int16();
+
+extern int uint16();
+
+__uint8_t au8, bu8, cu8;
+__uint16_t au16, bu16, cu16;
+__int8_t a8, b8, c8;
+__int16_t a16int, b16int, c16int;
+
 __int16_t chisl, znam, res;
+
+__uint8_t choose;
 
 int main() {
 
-    scanf("%hhu", &a);
-    scanf("%hhu", &b);
-    scanf("%hhu", &c);
+    setbuf(stdout, NULL);
 
-    mainA();
+    printf("Выберите тип входных данных: \n");
+    printf("1 - int8 \n");
+    printf("2 - uint8 \n");
+    printf("3 - int16 \n");
+    printf("4 - uint16 \n");
 
-    printf("числ: %hd \n", chisl);
-    printf("знам: %hd \n", znam);
-    printf("результат: %hd \n", res);
+    scanf("%hhu", &choose);
+
+    switch (choose) {
+        case 1:
+            scanf("%hhi", &a8);
+            scanf("%hhi", &b8);
+            scanf("%hhi", &c8);
+            int8();
+            break;
+        case 2:
+            scanf("%hhu", &au8);
+            scanf("%hhu", &bu8);
+            scanf("%hhu", &cu8);
+            uint8();
+            break;
+        case 3:
+            scanf("%hi", &a16int);
+            scanf("%hi", &b16int);
+            scanf("%hi", &c16int);
+            int16();
+            break;
+        case 4:
+            scanf("%hu", &au16);
+            scanf("%hu", &bu16);
+            scanf("%hu", &cu16);
+            uint16();
+            break;
+        default:
+            printf("Такого параметра");
+    }
+
+    printf("числитель: %hi \n", chisl);
+    printf("знаменатель: %hi \n", znam);
+    printf("результат: %hi \n", res);
 
     return 0;
 }
--- MDPL/lab1/main_asm.asm
+++ MDPL/lab1/main_asm.asm
@@ -1,16 +1,17 @@
 ; Define variables in the data section
 SECTION .DATA
-	extern a
-	extern b
-	extern c
+    extern au8, bu8, cu8
+    extern au16, bu16, cu16
+    extern a8, b8, c8
+    extern a16int, b16int, c16int;
 	extern chisl
 	extern znam
 	extern res
 
-global mainA
+global uint8, int8, uint16, int16
 
 ; (-74/a+c-5)/(1+c*b/2)
-mainA:
+uint8:
     push ax
     push dx
     push bx
@@ -22,14 +23,14 @@ mainA:
 
 ; -74/a
 	mov ax, -74d
-	mov bx, [a]
+	mov bx, [au8]
 	cwd
 	idiv bx
     mov [chisl], ax
 
 ; +c
     mov bx, 0
-    mov bl, [c]
+    mov bl, [cu8]
     add ax, bx
     mov [chisl], ax
 
@@ -43,13 +44,211 @@ mainA:
    	mov dx, 0
    	mov bx, 0
 
-	mov al, [b]
+	mov al, [bu8]
 	mov bx, 2d
 	idiv bx
     mov [znam], ax
 
 ; c*
-	mov al, [c]
+	mov al, [cu8]
+    imul ax
+    mov [znam], ax
+
+;1+
+    inc ax
+    mov [znam], ax
+
+; ------ деление (результат) ------
+; chisl/znam
+;
+    mov ax, 0
+    mov bx, 0
+    mov dx, 0
+
+    mov ax, [chisl]
+    mov bx, [znam]
+    cwd
+    idiv bx
+    mov [res], ax
+
+    pop bx
+    pop dx
+    pop ax
+ret
+
+; (-74/a+c-5)/(1+c*b/2)
+uint16:
+    push ax
+    push dx
+    push bx
+
+; --- числитель ---
+	mov ax, 0
+	mov dx, 0
+	mov bx, 0
+
+; -74/a
+	mov ax, -74d
+	mov bx, [au16]
+	cwd
+	idiv bx
+    mov [chisl], ax
+
+; +c
+    mov bx, 0
+    mov bl, [cu16]
+    add ax, bx
+    mov [chisl], ax
+
+; -5
+    sub ax, 5
+    mov [chisl], ax
+
+; ----- знаменатель ------
+; b/2
+    mov ax, 0
+   	mov dx, 0
+   	mov bx, 0
+
+	mov al, [bu16]
+	mov bx, 2d
+	idiv bx
+    mov [znam], ax
+
+; c*
+	mov al, [cu16]
+    imul ax
+    mov [znam], ax
+
+;1+
+    inc ax
+    mov [znam], ax
+
+; ------ деление (результат) ------
+; chisl/znam
+;
+    mov ax, 0
+    mov bx, 0
+    mov dx, 0
+
+    mov ax, [chisl]
+    mov bx, [znam]
+    cwd
+    idiv bx
+    mov [res], ax
+
+    pop bx
+    pop dx
+    pop ax
+ret
+
+; (-74/a+c-5)/(1+c*b/2)
+int16:
+    push ax
+    push dx
+    push bx
+
+; --- числитель ---
+	mov ax, 0
+	mov dx, 0
+	mov bx, 0
+
+; -74/a
+	mov ax, -74d
+	mov bx, [a16int]
+	cwd
+	idiv bx
+    mov [chisl], ax
+
+; +c
+    mov bx, 0
+    mov bl, [c16int]
+    add ax, bx
+    mov [chisl], ax
+
+; -5
+    sub ax, 5
+    mov [chisl], ax
+
+; ----- знаменатель ------
+; b/2
+    mov ax, 0
+   	mov dx, 0
+   	mov bx, 0
+
+	mov al, [b16int]
+	mov bx, 2d
+	idiv bx
+    mov [znam], ax
+
+; c*
+	mov al, [c16int]
+    imul ax
+    mov [znam], ax
+
+;1+
+    inc ax
+    mov [znam], ax
+
+; ------ деление (результат) ------
+; chisl/znam
+;
+    mov ax, 0
+    mov bx, 0
+    mov dx, 0
+
+    mov ax, [chisl]
+    mov bx, [znam]
+    cwd
+    idiv bx
+    mov [res], ax
+
+    pop bx
+    pop dx
+    pop ax
+ret
+
+; (-74/a+c-5)/(1+c*b/2)
+int8:
+    push ax
+    push dx
+    push bx
+
+; --- числитель ---
+	mov ax, 0
+	mov dx, 0
+	mov bx, 0
+
+; -74/a
+	mov ax, -74d
+	mov bx, [a8]
+	cwd
+	idiv bx
+    mov [chisl], ax
+
+; +c
+    mov bx, 0
+    mov bl, [c8]
+    add ax, bx
+    mov [chisl], ax
+
+; -5
+    sub ax, 5
+    mov [chisl], ax
+
+; ----- знаменатель ------
+; b/2
+    mov ax, 0
+   	mov dx, 0
+   	mov bx, 0
+
+	mov al, [b8]
+	mov bx, 2d
+	idiv bx
+    mov [znam], ax
+
+; c*
+	mov al, [c8]
     imul ax
     mov [znam], ax
 
