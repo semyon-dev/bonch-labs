fc285b33ccee66f9484599ab412fd04637de7ab3
semyon-dev <semyon-dev@protonmail.com>
2020-04-14 18:09:24 +0300

    bugs fixed & refactor

--- MDPL/lab1/main.c
+++ MDPL/lab1/main.c
@@ -10,12 +10,20 @@ extern int uint16();
 
 __uint8_t au8, bu8, cu8;
 __uint16_t au16, bu16, cu16;
-__int8_t a8, b8, c8;
+__int8_t a8int, b8int, c8int;
 __int16_t a16int, b16int, c16int;
 
 __int16_t chisl, znam, res;
+__int16_t chislC, znamC, resC;
 
 __uint8_t choose;
+
+void check(__int16_t a, __int16_t b, __int16_t c) {
+    // (-74/a+c-5)/(1+c*b/2)
+    chislC = ((-74 / a) + c - 5);
+    znamC = (1 + c * b / 2);
+    resC = chislC / znamC;
+}
 
 int main() {
 
@@ -29,38 +37,50 @@ int main() {
 
     scanf("%hhu", &choose);
 
+    printf("Теперь введите a,b,c: \n");
+
     switch (choose) {
         case 1:
-            scanf("%hhi", &a8);
-            scanf("%hhi", &b8);
-            scanf("%hhi", &c8);
+            scanf("%hhi", &a8int);
+            scanf("%hhi", &b8int);
+            scanf("%hhi", &c8int);
+            check(a8int, b8int, c8int);
             int8();
             break;
         case 2:
             scanf("%hhu", &au8);
             scanf("%hhu", &bu8);
             scanf("%hhu", &cu8);
+            check(au8, bu8, cu8);
             uint8();
             break;
         case 3:
             scanf("%hi", &a16int);
             scanf("%hi", &b16int);
             scanf("%hi", &c16int);
+            check(a16int, b16int, c16int);
             int16();
             break;
         case 4:
             scanf("%hu", &au16);
             scanf("%hu", &bu16);
             scanf("%hu", &cu16);
+            check(au16, bu16, cu16);
             uint16();
             break;
         default:
-            printf("Такого параметра");
+            printf("Такого параметра нет \n");
+            return 0;
     }
 
-    printf("числитель: %hi \n", chisl);
-    printf("знаменатель: %hi \n", znam);
-    printf("результат: %hi \n", res);
+    printf("Решаем: (-74/a+c-5)/(1+c*b/2) \n");
+    printf("Числитель: %hi \n", chisl);
+    printf("Знаменатель: %hi \n", znam);
+    printf("Результат: %hi \n", res);
+    printf("Проверка через си: \n");
+    printf("Числитель: %hi \n", chislC);
+    printf("Знаменатель: %hi \n", znamC);
+    printf("Результат: %hi \n", resC);
 
     return 0;
 }
--- MDPL/lab1/main_asm.asm
+++ MDPL/lab1/main_asm.asm
@@ -2,13 +2,97 @@
 SECTION .DATA
     extern au8, bu8, cu8
     extern au16, bu16, cu16
-    extern a8, b8, c8
-    extern a16int, b16int, c16int;
+    extern a8int, b8int, c8int
+    extern a16int, b16int, c16int
 	extern chisl
 	extern znam
 	extern res
 
-global uint8, int8, uint16, int16
+global int8, uint8, uint16, int16
+
+; (-74/a+c-5)/(1+c*b/2)
+int8:
+    push ax
+    push dx
+    push bx
+
+; --- числитель ---
+	mov ax, 0
+	mov dx, 0
+	mov bx, 0
+	mov al, 0
+	mov dl, 0
+
+    ;-74/a
+	mov ax, -74d        ; -74 -> ax
+	mov dl, [a8int]     ; a8int -> dl
+	; если делитель размером в байт, то делимое должно быть расположено в регистре ax.
+	; После операции частное помещается в al, а остаток — в ah;
+	idiv dl   ; ax / dl = al
+	cbw       ; (cbw конвертирует al -> ax)
+    mov [chisl], ax
+
+   ;+c
+    mov bx, 0
+    mov al, 0
+    mov ax, 0
+
+    mov bx, [chisl]
+    mov al, [c8int]
+    cbw ; (cbw конвертирует al -> ax)
+    add ax, bx
+    mov [chisl], ax
+
+ ;-5
+    sub ax, 5
+    mov [chisl], ax
+
+; ----- знаменатель ------
+; b/2
+    mov ax, 0
+   	mov dx, 0
+   	mov bx, 0
+   	mov al, 0
+
+	mov al, [b8int]
+	cbw   ; (cbw конвертирует al -> ax)
+	mov bl, 2d
+	idiv bl
+	; если делитель размером в байт, то делимое должно быть расположено в регистре ax.
+	; После операции частное помещается в al, а остаток — в ah;
+	cbw  ; (cbw конвертирует al -> ax)
+    mov [znam], ax
+
+ ; c*
+    mov ax, 0
+    mov bx, [znam]
+	mov al, [c8int]
+	cbw ; (cbw конвертирует al -> ax)
+    imul bx ; если операнд, указанный в команде, — слово, то второй сомножитель располагается в ax;
+    ; при умножении слов результат помещается в пару dx:ax;
+    mov [znam], ax
+
+    ;1+
+    inc ax
+    mov [znam], ax
+
+; ------ деление (результат) ------
+; chisl/znam
+;
+    mov ax, 0
+    mov bx, 0
+    mov dx, 0
+
+    mov ax, [chisl]
+    mov bx, [znam]
+    cwd
+    idiv bx
+    mov [res], ax
+
+    pop bx
+    pop dx
+    pop ax
+ret
 
 ; (-74/a+c-5)/(1+c*b/2)
 uint8:
@@ -23,9 +107,9 @@ uint8:
 
 ; -74/a
 	mov ax, -74d
-	mov bx, [au8]
-	cwd
-	idiv bx
+	mov dl, [au8]
+	idiv dl   ; ответ (частное) -> al
+	cbw       ; (cbw конвертирует al -> ax)
     mov [chisl], ax
 
 ; +c
@@ -50,8 +134,12 @@ uint8:
     mov [znam], ax
 
 ; c*
-	mov al, [cu8]
-    imul ax
+    mov ax, 0
+    mov bx, [znam]
+    mov al, [cu8]
+    cbw ; (cbw конвертирует al -> ax)
+    imul bx ; если операнд, указанный в команде, — слово, то второй сомножитель располагается в ax;
+            ; при умножении слов результат помещается в пару dx:ax;
     mov [znam], ax
 
 ;1+
@@ -110,14 +198,20 @@ uint16:
    	mov dx, 0
    	mov bx, 0
 
-	mov al, [bu16]
-	mov bx, 2d
-	idiv bx
+    mov bx, 2d
+    mov ax, [bu16]
+    cwd        ; преобразовать слово, находящееся в регистре АХ, в двойное слово — регистры DX:AX
+    idiv bx
+            ; если делитель размером в слово,
+            ; то делимое должно быть расположено в паре регистров dx:ax,
+            ; причем младшая часть делимого находится в ax.
+            ; После операции частное помещается в ax, а остаток — в dx;
     mov [znam], ax
 
 ; c*
-	mov al, [cu16]
-    imul ax
+   	mov bx, 0
+	mov bx, [cu16]
+    imul bx
     mov [znam], ax
 
 ;1+
@@ -155,14 +249,18 @@ int16:
 
 ; -74/a
 	mov ax, -74d
+	cwd ; преобразовать слово, находящееся в регистре АХ, в двойное слово — регистры < DX:AX >).
 	mov bx, [a16int]
-	cwd
 	idiv bx
+	    ; если делитель размером в слово,
+        ; то делимое должно быть расположено в паре регистров dx:ax,
+        ; причем младшая часть делимого находится в ax.
+        ; После операции частное помещается в ax, а остаток — в dx;
     mov [chisl], ax
 
 ; +c
     mov bx, 0
-    mov bl, [c16int]
+    mov bx, [c16int]
     add ax, bx
     mov [chisl], ax
 
@@ -176,89 +274,30 @@ int16:
    	mov dx, 0
    	mov bx, 0
 
-	mov al, [b16int]
 	mov bx, 2d
+	mov ax, [b16int]
+	cwd        ; преобразовать слово, находящееся в регистре АХ, в двойное слово — регистры DX:AX
 	idiv bx
+	        ; если делитель размером в слово,
+            ; то делимое должно быть расположено в паре регистров dx:ax,
+            ; причем младшая часть делимого находится в ax.
+            ; После операции частное помещается в ax, а остаток — в dx;
     mov [znam], ax
 
-; c*
-	mov al, [c16int]
-    imul ax
-    mov [znam], ax
-
-;1+
-    inc ax
-    mov [znam], ax
-
-; ------ деление (результат) ------
-; chisl/znam
-;
-    mov ax, 0
-    mov bx, 0
-    mov dx, 0
-
-    mov ax, [chisl]
-    mov bx, [znam]
-    cwd
-    idiv bx
-    mov [res], ax
-
-    pop bx
-    pop dx
-    pop ax
-ret
-
-; (-74/a+c-5)/(1+c*b/2)
-int8:
-    push ax
-    push dx
-    push bx
-
-; --- числитель ---
-	mov ax, 0
-	mov dx, 0
-	mov bx, 0
-
-; -74/a
-	mov ax, -74d
-	mov bx, [a8]
-	cwd
-	idiv bx
-    mov [chisl], ax
-
-; +c
-    mov bx, 0
-    mov bl, [c8]
-    add ax, bx
-    mov [chisl], ax
-
-; -5
-    sub ax, 5
-    mov [chisl], ax
-
-; ----- знаменатель ------
-; b/2
-    mov ax, 0
-   	mov dx, 0
+    ; c*
    	mov bx, 0
-
-	mov al, [b8]
-	mov bx, 2d
-	idiv bx
+	mov bx, [c16int]
+    imul bx
     mov [znam], ax
 
-; c*
-	mov al, [c8]
-    imul ax
-    mov [znam], ax
-
-;1+
+    ;1+
     inc ax
     mov [znam], ax
 
 ; ------ деление (результат) ------
-; chisl/znam
-;
+
+    ;chisl/znam
+
     mov ax, 0
     mov bx, 0
     mov dx, 0
