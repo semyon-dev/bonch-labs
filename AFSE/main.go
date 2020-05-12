86e89252149ee906c9a684562ed5b97fc001e30c
semyon-dev <semyon-dev@protonmail.com>
2020-05-13 02:18:29 +0300

    feat: 2 method in AFSE lab1

--- AFSE/main.go
+++ AFSE/main.go
@@ -6,15 +6,27 @@ import (
 	"os"
 )
 
-var a, b, c, d, x1, x2 float64
+var a, b, c, d float64
+var a1, b1, x float64
 
-const E = 0.01 // погрешность
+var E = 0.01 // погрешность
 
+// уравнение
 func f(x float64) float64 {
 	return a*math.Pow(x, 3) + b*math.Pow(x, 2) + c*x + d
 }
 
+// производная от уравнения
+func fp(x float64) float64 {
+	return 3*a*math.Pow(x, 2) + 2*b*x + c
+}
+
 func main() {
+	fmt.Println("выберите метод решения")
+	fmt.Println("метод секущих - 0")
+	fmt.Println("метод касательных - 1")
+	var method int
+	fmt.Scan(&method)
 	fmt.Println("Введите a,b,c,d")
 	_, err := fmt.Scan(&a, &b, &c, &d)
 	if err != nil {
@@ -30,20 +42,56 @@ func main() {
 			os.Exit(0)
 		}
 	}
-	fmt.Println("Введите x1, x2") // начальное приближение x1, начальное приближение x2
-	_, err = fmt.Scan(&x1, &x2)
-	if err != nil {
-		fmt.Println("Неправильный формат данных!")
+
+	fmt.Println("Введите границы отрезка [a;b]")
+	fmt.Scan(&a1, &b1)
+	fmt.Println("Введите значение погрешности")
+	fmt.Scan(&E)
+	if a1 > b1 {
+		fmt.Println("Неверно указан интервал поиска")
 		os.Exit(0)
 	}
+	if E < 0 {
+		fmt.Println("Значение погрешности должно быть положительным")
+		os.Exit(0)
+	}
+
+	switch method {
+	case 0:
+		secant() // метод секущих
+	case 1:
+		tangents() // метод касательныx
+	default:
+		fmt.Println("Такого метода нет!")
+	}
+}
+
+// метод касательных
+func tangents() {
+	x = b1
+	fmt.Println("Корень функции: ", Newton(x, E))
+}
+
+func Newton(x0, epsilon float64) float64 {
+	f0 := f(x0)
+	x := x0
+	for math.Abs(f(x)) > epsilon {
+		x -= f0 / fp(x)
+		f0 = f(x)
+	}
+	return x
+}
+
+// метод секущих
+func secant() {
 	var y, xN float64
 	var n = 0
 	for {
 		n++
 		y = xN
-		xN = x2 - ((x2-x1)/(f(x2)-f(x1)))*f(x2)
-		x1 = x2
-		x2 = xN
+		xN = b1 - ((b1-a1)/(f(b1)-f(a1)))*f(b1)
+		a1 = b1
+		b1 = xN
 		if !(math.Abs(y-xN) >= E) {
 			break
 		}
