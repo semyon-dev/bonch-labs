4cbed39df99899b3bedd3c1a70f139aa05792a7d
semyon-dev <semyon-dev@protonmail.com>
2020-05-12 20:21:11 +0300

    feat: programming: lab12

--- Programming/lab12/lab12.c
+++ Programming/lab12/lab12.c
@@ -1,4 +1,28 @@
 //
 // Created by semyon on 12.05.2020.
 //
+// Написать функцию пользователя, позволяющую выполнять решение задачи табулирования для произвольной функции одного переменного.
+// Табулируемая функция передается через параметр табулирующей функции.
+// Результаты табулирования выводятся на экран монитора.
+#include <stdio.h>
 
+typedef double(*function)(double x);
+
+void tab(function f) {
+    for (double i = -1000; i < 1000; i++) {
+        printf("%lf \n", f(i));
+    }
+}
+
+double f(double x) {
+    return x * 2;
+}
+
+double f2(double x) {
+    return x * x;
+}
+
+int main() {
+    tab(&f);
+    tab(&f2);
+}
